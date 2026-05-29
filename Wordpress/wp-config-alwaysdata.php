01f2cabdaa134e219ae81cb7aa8ae3f595b36d0a
Willem Dulormne <willem.dulormne@gmail.com>
2026-05-29 19:52:42 +0200

    chore: clean up temporary deployment files and update wp-config

--- Wordpress/wp-config-alwaysdata.php
+++ Wordpress/wp-config-alwaysdata.php
@@ -22,7 +22,7 @@ define( 'DB_PASSWORD', 'Kirua777' );
 define( 'DB_HOST', 'mysql-dulormne.alwaysdata.net' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
